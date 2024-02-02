4cf090102c07a3e2b7f9d054c2281de0c54e2c20
rahul-17-11 <rahulw1711@gmail.com>
2024-02-02 11:55:13 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './componets/Navbar';
 import Textarea from './componets/Textarea';
 // import About from './componets/About';
 
 function App() {
+  const [mode,setMode] = useState("light")
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode('dark')
+      document.body.style.backgroundColor = '#072d54';
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="ONE PIECE" about="AboutUs"/>
+      <Navbar title="ONE PIECE" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3 my-3">
-      <Textarea heading="Enter your text below"/>
+      <Textarea heading="Enter your text below" mode={mode}/>
       {/* <About/> */}
       </div>
     </>
--- src/componets/Textarea.js
+++ src/componets/Textarea.js
@@ -12,38 +12,20 @@ export default function Textarea(props) {
     const clickOnClear = ()=>{
         setText("")
     }
-    const [colorChange,setColorChange] = useState({
-        color:'black'
-    })
-    const [btnc,setBtnc] = useState("Change color to red")
-    const colorChanger = ()=>{
-        if(colorChange.color === 'red'){
-            setColorChange({
-                color:'black'
-            })
-            setBtnc("Change color to red")
-        }else{
-            setColorChange({
-                color:'red'
-            })
-            setBtnc("Change color to black")
-        }
-    }
     const handleOnChange = (event)=>{
         setText(event.target.value)
     }
     const [text,setText] = useState("Some reference text")
   return (
         <>
-        <div className="mb-3 my-3">
+        <div className="mb-3 my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
-        <textarea className="form-control" value={text} style={{ color: colorChange.color }} onChange={handleOnChange} id="myText" rows="10"></textarea>
+        <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'#233240':'white',color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myText" rows="10"></textarea>
         <button className="btn btn-primary my-3 mx-2" onClick={handleOnClick}>Click Here for upper case</button>
         <button className="btn btn-primary my-3 mx-2" onClick={clickOnHandle}>Click here for lower case</button>
         <button className="btn btn-primary my-3 mx-2" onClick={clickOnClear}>Clear</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={colorChanger}>{btnc}</button>
         </div>
-        <div className='container '>
+        <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Summary of the above text box</h2>
         <p>{text.split(" ").length} words and {text.length} characters in the above text box.</p>
         <p>You will require aprroximately {0.008* text.split(" ").length} minutes to read this text.</p>
